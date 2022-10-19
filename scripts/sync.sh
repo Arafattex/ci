f37d67971d8f12c17cecd7ccc5a958051d2f10ee
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-10-19 17:37:02 +0600

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -19,9 +19,23 @@ telegram_message() {
 # Clone the Sync Repo
 
 # Initialize local repository
-repo init -u https://github.com/PixelExperience/manifest -b thirteen
-curl -o .repo/local_manifests/local_manifests.xml https://raw.githubusercontent.com/Arafattex/Local_manifest_mt6768/Lancelot_pixel/13.xml --create-dirs
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-
+cd ~
+git clone https://github.com/Arafattex/shas-dream-oc-mt6768-a11.git --depth 1 -b shas-noc kernel
+cd kernel
+git clone https://github.com/rama982/clang --depth=1 clang
+mkdir outL
+export ARCH=arm64
+export SUBARCH=arm64
+export DTC_EXT=dtc
+make O=outL ARCH=arm64 lancelot_defconfig
+export PATH="~/kernel/clang/bin:${PATH}"
+make -j"$PROCS" O=outL \
+                  ARCH=arm64 \
+                  CC=clang \
+                  CROSS_COMPILE=aarch64-linux-gnu- \
+                  CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                  LD=ld.lld \
+                  NM=llvm-nm \
+                  OBJCOPY=llvm-objcopy
 # Exit
 exit 0
